047734ba7591b398b24a2504adc13362079a3404
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-17 14:15:39 -0300

    def pedido retirada

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from cliente import Cliente
 from item import Item
+from pedido.pedido_retirada import PedidoRetirada
+
 
 cliente = Cliente("kleber", "SP")
 item_um = Item("Pizza", 30.0)
 item_dois = Item("Regrigerante", 5.0)
+itens = [item_um, item_dois]
+
+pedido_retirada = PedidoRetirada(cliente, itens)
+print(f"Preco do pedido retirada: {pedido_retirada.calcular_total():.2f}")
