7102180bf63404f43bc1eac35660df3d20e26079
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-17 14:58:53 -0300

    def pedido delivery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cliente import Cliente
 from item import Item
 from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delivery import PedidoDelivery
 
 
 cliente = Cliente("kleber", "SP")
@@ -9,4 +10,7 @@ item_dois = Item("Regrigerante", 5.0)
 itens = [item_um, item_dois]
 
 pedido_retirada = PedidoRetirada(cliente, itens)
-print(f"Preco do pedido retirada: {pedido_retirada.calcular_total():.2f}")
+
+taxa_entrega = 10.0
+pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
+print(f"Preco do pedido delivery: {pedido_delivery.calcular_total():.2f}")
