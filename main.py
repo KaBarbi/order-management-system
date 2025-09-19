956fc40ce262685dafd36bd88166e9d0f6c20a3b
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-19 13:21:17 -0300

    pagamento

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from cliente import Cliente
 from item import Item
 from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delivery import PedidoDelivery
+from pagamento.pagamento_cartao import PagamentoCartao
+from pagamento.pagamento_pix import PagamentoPIX
 
 
 cliente = Cliente("kleber", "SP")
@@ -13,4 +15,8 @@ pedido_retirada = PedidoRetirada(cliente, itens)
 
 taxa_entrega = 10.0
 pedido_delivery = PedidoDelivery(cliente, itens, taxa_entrega)
-print(f"Preco do pedido delivery: {pedido_delivery.calcular_total():.2f}")
+
+valor_pedido = pedido_delivery.calcular_total()
+pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+pagamento_pix = PagamentoPIX()
+pagamento_pix.processar(valor_pedido)
